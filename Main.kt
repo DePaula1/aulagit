95f4586b4e018e9efd9e79ea3d4bc12f339b99e9
lucas de paula <lucaspaulade@gmail.com>
2024-06-03 10:48:37 -0300

    Declaração de variavel
    Realizada a implementação da variavel name e incorporado na função main

--- Main.kt
+++ Main.kt
@@ -1,3 +1,11 @@
+//val -> declaração de variavel somente leitura
+//var -> declaração de variavel mutavel
+//listOf -> criar uma lista somente leitura
+//MutableListOf -> criar uma lista mutavel
+
+
+val readNames = listOf("Davi","João","André")
+
 fun main (){
     println("Hello, World!")
 }
