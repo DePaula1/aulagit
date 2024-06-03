a3e20863d8b99ce1f71c0b2347ea0cf893481bf2
lucas de paula <lucaspaulade@gmail.com>
2024-06-03 10:56:00 -0300

    Inserção da lista readNames
    Inserido uma lista para armazenar o nome dos usuários de modo estático.

--- Main.kt
+++ Main.kt
@@ -1,7 +1,9 @@
 //val -> declaração de variavel somente leitura
 //var -> declaração de variavel mutavel
+//listOf -> Criar uma lista somente leitura
+//MutableListOf -> criar uma lista mutavel
 
-val name = "Lucas"
+val readName = listOf("Davi","João","André")
 
 fun main (){
     println("Hello, $name")
