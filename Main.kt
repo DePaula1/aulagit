710bcde9e96b11394b3cdd2b45f6939f45fdf51b
lucas de paula <lucaspaulade@gmail.com>
2024-06-03 11:08:41 -0300

    Atualização first commit
    Atualizando o first commit pois tinha erro

--- Main.kt
+++ Main.kt
@@ -1,10 +1,7 @@
 //val -> declaração de variavel somente leitura
 //var -> declaração de variavel mutavel
-//listOf -> criar uma lista somente leitura
-//MutableListOf -> criar uma lista mutavel
 
-
-val readNames = listOf("Davi","João","André")
+val name = "Davi"
 
 fun main (){
     println("Hello, World!")
